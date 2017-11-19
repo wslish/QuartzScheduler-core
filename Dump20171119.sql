fddd70f29448f1ca4414910d674e30baf9f38ca6
lishuai <wslish@qq.com>
2017-11-19 17:58:21 +0800

    add db script

---
+++ Dump20171119.sql
@@ -0,0 +1,99 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: quartz_scheduler
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `setting`
+--
+
+DROP TABLE IF EXISTS `setting`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `setting` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(32) NOT NULL,
+  `key` varchar(64) NOT NULL,
+  `value` varchar(4096) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `job_group` varchar(32) NOT NULL,
+  `level` smallint(6) NOT NULL DEFAULT '1',
+  `dependency_task_id` varchar(64) DEFAULT '',
+  `dependency_status` smallint(6) DEFAULT '1',
+  `spec` varchar(64) DEFAULT NULL,
+  `protocol` tinyint(4) NOT NULL,
+  `command` varchar(256) NOT NULL,
+  `timeout` mediumint(9) NOT NULL DEFAULT '0',
+  `multi` tinyint(4) NOT NULL DEFAULT '1',
+  `notify_status` smallint(6) DEFAULT '1',
+  `notify_receiver` varchar(256) DEFAULT '',
+  `cron` varchar(32) NOT NULL DEFAULT '',
+  `remark` varchar(100) DEFAULT '',
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `is_durable` int(11) NOT NULL DEFAULT '1' COMMENT '0 否，1是',
+  `create_time` datetime NOT NULL,
+  `is_deleted` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `IDX_gocrontask_protocol` (`protocol`),
+  KEY `IDX_gocrontask_status` (`status`),
+  KEY `IDX_gocrontask_level` (`level`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task_log`
+--
+
+DROP TABLE IF EXISTS `task_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `task_id` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(32) NOT NULL,
+  `command` varchar(256) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  `result` mediumtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_gocrontask_log_task_id` (`task_id`),
+  KEY `IDX_gocrontask_log_status` (`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-19 17:57:18
